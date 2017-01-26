29f83756688d92feedb64c871df238831f16c62d
Sergio <sergio.decandelario@gmai.com>
2017-01-26 20:42:14 +0100

    Adapted the code to pass all the tests

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -9,9 +9,25 @@ namespace SergioDeCandelario\BowlingKata;
  */
 class BowlingGame
 {
+    /**
+     * @var int
+     */
     private $score;
 
+    /**
+     * @var array
+     */
     private $rolls;
+
+    /**
+     * @var array
+     */
+    private $extraScore;
+
+    /**
+     * @var
+     */
+    private $totalRolls;
 
     /**
      * BowlingGame constructor.
@@ -21,49 +37,81 @@ class BowlingGame
     public function __construct(array $rolls)
     {
         $this->rolls = $rolls;
+        $this->totalRolls = 0;
         $this->score = 0;
+        $this->extraScore = [];
     }
 
+    /**
+     * @return int
+     */
     public function score()
     {
         return $this->score;
     }
 
+    /**
+     *
+     */
     public function calculateScore()
     {
+        $this->initializeExtraScore();
+
         $previousRoll = 0;
-        $additionalTurns = 0;
         $launch = 1;
-        $totalRolls = count($this->rolls);
 
-        foreach ($this->rolls as $turn => $roll) {
+        foreach ($this->rolls as $roll => $score) {
 
-            if ($additionalTurns && $turn < ($totalRolls - $additionalTurns)) {
-                $rounds = ($turn == 1) ? 1 : $additionalTurns;
+            if ($multiplier = $this->extraScore[$roll]) {
+                $this->score += $score * $multiplier;
+            }
 
-                for ($i = 0; $i < $rounds; $i++) {
-                    $this->score += $roll;
+            if (($previousRoll + $score == 10) && $launch == 2) {
+                if (($roll + 2) < $this->totalRolls) {
+                    $this->addExtraScore($roll + 1);
                 }
-
-                $additionalTurns--;
             }
 
-            if (($previousRoll + $roll == 10) && $launch == 2) {
-                $additionalTurns = 1;
+            if ($score == 10) {
+                if (($roll + 3) < $this->totalRolls) {
+                    $this->addExtraScore($roll + 1);
+                    $this->addExtraScore($roll + 2);
+                }
             }
 
-            if ($roll == 10) {
-                $additionalTurns = 2;
-            }
-
-            if ($roll == 10 || $launch == 2) {
+            if ($score == 10 || $launch == 2) {
                 $launch = 0;
             }
 
-            $previousRoll = $roll;
+            $previousRoll = $score;
             $launch++;
 
-            $this->score += $roll;
+            $this->score += $score;
+        }
+    }
+
+    /**
+     *
+     */
+    private function initializeExtraScore()
+    {
+        $this->totalRolls = count($this->rolls);
+
+        $i = 0;
+
+        do {
+            $this->extraScore[] = 0;
+            $i++;
+        } while ($i < $this->totalRolls);
+    }
+
+    /**
+     * @param $position
+     */
+    private function addExtraScore($position)
+    {
+        if (isset($this->extraScore[$position])) {
+            $this->extraScore[$position] += 1;
         }
     }
 }
